cb6c157c0cd833e01366b50b6158aa57cb7776ae
kyleredilla <kmredilla@alaska.edu>
2023-07-20 14:06:37 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# analog-forecast-profiling-viz
+
+This repo is a test run for visualizing some results from the analog forecast profiling efforts. Click the link below to launch a notebook to view some of the results.
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/ua-snap/analog-forecast-viz/main?filepath=error_viz.ipynb)
---
+++ error_viz.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+import ipywidgets as widgets
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+analog_df = pd.read_csv("analog_profiling_results_20230719.csv")
+naive_df = pd.read_csv("naive_profiling_results_20230719.csv")
+
+varnames_rb = widgets.RadioButtons(
+    value="t2m", 
+    options=["sst", "t2m", "msl", "z"], 
+    description="Variable name"
+)
+search_domains_rb = widgets.RadioButtons(
+    value="alaska", 
+    options=["alaska", "northern_hs", "north_pacific", "panarctic"], 
+    description="Search domain"
+)
+forecast_domains_rb = widgets.RadioButtons(
+    value="alaska", 
+    options=["alaska", "northern_hs", "north_pacific", "panarctic"], 
+    description="Forecast domain"
+)
+
+controls = widgets.HBox([
+    varnames_rb, 
+    search_domains_rb, 
+    forecast_domains_rb
+])
+
+
+# @widgets.interact(
+#     varname=varnames_rb, 
+#     search_domain=search_domains_rb, 
+#     forecast_domain=forecast_domains_rb,
+#     ref_date=analog_df.reference_date.unique(),
+# )
+def plot_error(varname, search_domain, forecast_domain, ref_date):
+    fig, ax = plt.subplots(1, 1, figsize=(9, 5))
+    query_str = (
+        "forecast_domain == @forecast_domain "
+        "& search_domain == @search_domain "
+        "& variable == @varname "
+        "& reference_date == @ref_date"
+    )
+    
+    naive_query_str = query_str.replace("& search_domain == @search_domain", " ")
+    plot_df = naive_df.query(
+        # only forecast_domain in naive_df
+        naive_query_str
+    )[["forecast_date", "naive_2.5", "naive_50", "naive_97.5"]].set_index("forecast_date")
+    
+    analog_query_str = query_str + " & anomaly_search == True"
+    plot_df = plot_df.join(
+        analog_df.query(analog_query_str)[[
+            "forecast_date", "forecast_error"
+        ]].set_index("forecast_date").rename(
+            columns={"forecast_error": "anom_forecast_error"}
+        )
+    )
+    
+    anom_query_str = analog_query_str.replace("anomaly_search == True", "anomaly_search == False")
+    plot_df = plot_df.join(
+        analog_df.query(anom_query_str)[[
+            "forecast_date", "forecast_error"
+        ]].set_index("forecast_date").rename(
+            columns={"forecast_error": "forecast_error"}
+        )
+    )
+    
+    plot_df.plot(title=f"{varname},  search={search_domain}, forecast={forecast_domain},  Reference Date: {ref_date}", ax=ax)
+    ax.set_ylabel("RMSE")
+    
+#     plt.show()
+    
+    return
+
+
+# output = widgets.Output()
+# app = widgets.HBox([controls, output])
+
+
+widget = widgets.interactive(plot_error, varname=varnames_rb, search_domain=search_domains_rb, forecast_domain=forecast_domains_rb, ref_date=analog_df.reference_date.unique())
+output = widget.children[-1]
